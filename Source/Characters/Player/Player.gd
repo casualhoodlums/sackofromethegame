6c63f1ff4fca339eaacafddc3d8fe7a20434d21e
Kyle McCahill <kylemccahill@gmail.com>
2023-11-14 21:02:54 -0600

    updated gitignore, player rotation, added placeholder nodes

--- Source/Characters/Player/Player.gd
+++ Source/Characters/Player/Player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 @export var SPEED = 200.0
 @export var ACCELERATION = 1000.0
-@export var FRICTION = 500.0
+@export var FRICTION = 600.0
 
 @export var MAX_VELOCITY = Vector2(200.0, 200.0)
 @export var MIN_VELOCITY = Vector2.ZERO
+
+@onready var player_sprite = $PlayerSprite
+@onready var player_collison = $PlayerCollison
+@onready var camera_2d = $Camera2D
+
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,14 +19,14 @@ func _physics_process(delta):
 	
 	input_vector = input_vector.normalized()
 	if input_vector:
-		# velocity = velocity.move_toward(input_vector * SPEED, ACCELERATION * delta)
+		rotation = input_vector.angle() + (PI / 2)
 		velocity.x = move_toward(velocity.x, SPEED * input_vector.x, ACCELERATION * delta)
 		velocity.y = move_toward(velocity.y, SPEED * input_vector.y, ACCELERATION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		velocity.y = move_toward(velocity.y, 0, FRICTION * delta)
 		
-		
+	
 	print(velocity)
 	move_and_slide()
 	# # Get the input direction and handle the movement/deceleration.
