2faaa2b83b5c825a002a4dd4d0282eebd39cec62
Saint762 <egibson762@gmail.com>
2023-11-15 21:31:34 -0700

    dev menu

--- source/World.gd
+++ source/World.gd
@@ -7,9 +7,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		if Input.is_action_just_pressed("exit"):
-			show_pause_menu()
+	if Input.is_action_just_pressed("exit"):
+		show_pause_menu()
+		
+	if Input.is_action_just_pressed("console"):
+		toggle_dev_menu()
 		
 func show_pause_menu():
 	get_tree().paused = true;
 	$PauseMenu/PauseCanvas.visible = true;
+	
+func toggle_dev_menu():
+	if $DevMenu/DevMenuCanvas.visible == false:
+		$DevMenu/DevMenuCanvas.visible = true;
+	else:
+		$DevMenu/DevMenuCanvas.visible = false;
---
+++ source/ui/DevMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_spawn_man_pressed():
+	print("OK IM SPAWNING")
+	# Interesting Code
+	
+
+func _on_invincible_pressed():
+	print("OK IM INVINCIBLE")
+	# Interesting Code
