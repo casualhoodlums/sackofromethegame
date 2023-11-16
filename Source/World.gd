7eae063c0a2f836f10a86e2c27c2898bccd36c1e
Saint762 <egibson762@gmail.com>
2023-11-15 17:25:06 -0700

    animations, art pack, menus and decorated the main level a bit

---
+++ Source/World.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		if Input.is_action_just_pressed("exit"):
+			show_pause_menu()
+		
+func show_pause_menu():
+	get_tree().paused = true;
+	$PauseMenu/PauseCanvas.visible = true;
